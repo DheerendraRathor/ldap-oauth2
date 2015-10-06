adf3a1e8868a9372972dadf6a9ba3ba6b09b4b33
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-10-06 21:04:11 +0530

    Bug fixed. 'basic' scope was not present in case of request_approval=auto

--- application/validators.py
+++ application/validators.py
@@ -4,6 +4,10 @@ from oauth2_provider.oauth2_validators import OAuth2Validator
 
 
 class CustomOAuth2Validator(OAuth2Validator):
-
     def get_default_scopes(self, client_id, request, *args, **kwargs):
         return ['basic']
+
+    def validate_scopes(self, client_id, scopes, client, request, *args, **kwargs):
+        if 'basic' not in request.scopes:
+            request.scopes.append('basic')
+        return super(CustomOAuth2Validator, self).validate_scopes(client_id, scopes, client, request, *args, **kwargs)
