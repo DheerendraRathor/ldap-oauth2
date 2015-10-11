7ded522fcc46873e0b1c3b38107c112bdaa6b1fa
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-10-11 05:39:55 +0530

    Enforced one app - one user - one access_token - one refresh_token

--- application/validators.py
+++ application/validators.py
@@ -1,4 +1,9 @@
+from datetime import timedelta
+
 from oauth2_provider.oauth2_validators import OAuth2Validator
+from oauth2_provider.models import RefreshToken, AccessToken
+from django.utils import timezone
+from oauth2_provider.settings import oauth2_settings
 
 
 class CustomOAuth2Validator(OAuth2Validator):
@@ -9,3 +14,52 @@ class CustomOAuth2Validator(OAuth2Validator):
         if 'basic' not in request.scopes:
             request.scopes.append('basic')
         return super(CustomOAuth2Validator, self).validate_scopes(client_id, scopes, client, request, *args, **kwargs)
+
+    def save_bearer_token(self, token, request, *args, **kwargs):
+        """
+        It's messy. It is 90% code from parent function. I didn't find a way to reduce it.
+        I tried and I failed :'(
+        """
+
+        """
+        Save access and refresh token, If refresh token is issued, remove old refresh tokens as
+        in rfc:`6`
+        """
+        if request.refresh_token:
+            # remove used refresh token
+            # Copied as is from parent. I don't know why they're even caring to delete this! - Dheerendra
+            try:
+                RefreshToken.objects.get(token=request.refresh_token).revoke()
+            except RefreshToken.DoesNotExist:
+                assert ()  # TODO though being here would be very strange, at least log the error
+
+        expires = timezone.now() + timedelta(seconds=oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS)
+        if request.grant_type == 'client_credentials':
+            request.user = None
+
+        access_token = AccessToken(
+            user=request.user,
+            scope=token['scope'],
+            expires=expires,
+            token=token['access_token'],
+            application=request.client)
+        access_token.save()
+
+        if 'refresh_token' in token:
+            refresh_token = RefreshToken(
+                user=request.user,
+                token=token['refresh_token'],
+                application=request.client,
+            )
+            if request.grant_type == 'authorization_code':
+                try:
+                    refresh_token = RefreshToken.objects.get(user=request.user, application=request.client)
+                    refresh_token.access_token.delete()
+                    # Deleted the old access_token
+                except RefreshToken.DoesNotExist:
+                    pass
+            refresh_token.access_token = access_token
+            refresh_token.save()
+            token['refresh_token'] = refresh_token.token
+
+        token['expires_in'] = oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS
