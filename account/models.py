7871546762084b2943c344c43bd334a9cf893c82
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-10-14 00:39:42 +0530

    Adding user profile picture support.
    1. Changed UI for user home page
    2. Added Profile Badge with basic stuff
    3. TODO: Crop image on client side
    4. Resize image on server side

--- account/models.py
+++ account/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+from uuid import uuid4
+
+def user_profile_picture(instance, filename):
+    filename = filename.split('.')
+    if len(filename) > 1:
+        ext = filename[-1]
+    else:
+        ext = "png"
+    return os.path.join("profile_picture", uuid4().hex + "." + ext)
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    profile_picture = models.ImageField(upload_to=user_profile_picture, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     roll_number = models.CharField(max_length=16, null=True, blank=True)
     type = models.CharField(max_length=16, null=True, blank=True)
---
+++ core/templatetags/model_media.py
@@ -0,0 +1,15 @@
+from django import template
+from django.conf import settings
+from urlparse import urljoin
+
+register = template.Library()
+
+
+@register.simple_tag()
+def model_field_media_url(field):
+    try:
+        url = field.url
+        url = urljoin(settings.MEDIA_URL, url)
+    except ValueError:
+        url = None
+    return url
